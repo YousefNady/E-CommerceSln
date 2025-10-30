ba0c97846c3f9a06b58b8c26aaa7a0e5e576b026
YousefNady <yousefnadi01@gmail.com>
2025-10-30 10:05:53 +0200

    ProductModuleWithConfigurations

---
+++ E Commerce.Persistence/Data/DbContexts/StoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Data.DbContexts
+{
+    public class StoreDbContext : DbContext
+    {
+        // dependency injection
+        // clr will inject the object 
+        // + need to add that services in program.cs
+        public StoreDbContext(DbContextOptions<StoreDbContext> Options) : base(Options)  
+        {
+            
+        }
+
+
+
+    }
+}
