c69d118b8b06c60fd88519bd2eaee0d778be7c71
YousefNady <yousefnadi01@gmail.com>
2025-10-30 10:14:17 +0200

    ApplyConfigurationsFromAssembly

--- E Commerce.Persistence/Data/DbContexts/StoreDbContext.cs
+++ E Commerce.Persistence/Data/DbContexts/StoreDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,8 +17,9 @@ namespace E_Commerce.Persistence.Data.DbContexts
         {
             
         }
-
-
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
