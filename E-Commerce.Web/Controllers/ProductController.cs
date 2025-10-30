403e3ad4bd55f7b036719b9513df6574940201aa
YousefNady <yousefnadi01@gmail.com>
2025-10-30 09:09:25 +0200

    Add project files.

---
+++ E-Commerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // Get: BaseURL/api/Product/10 
+        // No Need To Add EndPoint Name(Action)
+        // URL & Methodبيعرف الاند بوينت عن طريق ال 
+        // HttpGetهيدور في الكنترول علي اند بوينت بتاخد ميسود 
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            return new Product { Id = id, Name = "Test" };
+        
+        }
+
+        // Get: BaseURL/api/Product
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll()
+        {
+            return new List<Product>();
+        }
+    }
+}
---
+++ E-Commerce.Web/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
